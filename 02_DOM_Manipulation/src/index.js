79d392a61dacecc4e45f224a4518e4515f53d8bc
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2022-11-01 10:45:37 -0700

    update comments in index.js

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -6,6 +6,11 @@ function priceFormatter(priceNum) {
   return '$' + priceNum.toFixed(2);
 }
 
+// create a function renderHeader() that takes the store name from bookStore and adds to the DOM
+
+// create a function renderFooter() that takes the store address from bookStore and adds to the DOM
+
+
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
@@ -20,4 +25,3 @@ function priceFormatter(priceNum) {
   //     <img src="https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" alt="Eloquent JavaScript cover"/>
   //   </li>
 
-  
